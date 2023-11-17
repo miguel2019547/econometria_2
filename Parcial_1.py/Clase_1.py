0f587d8ffce562b9049aa4d6c5769f05c0517d6c
Miguelarnomics <54946811+miguel2019547@users.noreply.github.com>
2023-11-16 20:19:06 -0400

    Se agregan librerias

--- Parcial_1.py/Clase_1.py
+++ Parcial_1.py/Clase_1.py
@@ -1,1 +1,10 @@
-print("hola mundo")
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_poisson_deviance, max_error
+from statsmodels.tsa.arima.model import ARIMA
+
+#----------------------------------------------------------------------------------------------------
+
+#                               
