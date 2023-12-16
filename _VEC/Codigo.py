f7de0ac7d8cd8d18511eda3ffb473f04438f8e1d
Miguelarnomics <54946811+miguel2019547@users.noreply.github.com>
2023-12-16 16:41:39 -0400

    Vec model actualizado

--- _VEC/Codigo.py
+++ _VEC/Codigo.py
@@ -3,6 +3,7 @@ from statsmodels.tsa.api import VAR
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tools.eval_measures import rmse
 from itertools import permutations, product
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
 
 def Tachos(data):
     data = pd.read_csv(data)
@@ -20,3 +21,31 @@ def Columnas(data, Busqueda):
     Palabra = [cadena for cadena in data.columns if Busqueda in cadena]
     for a in Palabra:
         print(a)
+
+
+def Johansen_Test(data, det_order, k_ar_diff, conf = 1):
+    result = coint_johansen(data, det_order= det_order, k_ar_diff= k_ar_diff)
+    a95 = result.cvm[conf,:]
+    eig =result.lr2
+    print('=================================================')
+    print('Matriz de Valores Criticos vs Maximo Valor Propio')
+    print('=================================================')
+    print(f'            cvm     eigv        Hipotesis')
+    for i in range(len(data.columns)):
+        if eig[i] > a95[i]:
+            sr = f'Se rechaza r = {i}, existe mas de {i} relación/nes de cointegración'
+        else:
+            sr = f'No hay suficiente evidencia para rechazar la hipótesis nula de r = {i}.'
+        print(f'r = {i}     {a95[i]}    {round(eig[i], 4)}    {sr}')
+    a95 = result.cvt[conf,:]
+    eig =result.lr1
+    print('=================================================')
+    print('Tabla de Valores Criticos vs Traza Estadistica')
+    print('=================================================')
+    print(f'            cvt     TS          Hipotesis')
+    for i in range(len(data.columns)):
+        if eig[i] > a95[i]:
+            sr = f'Se rechaza r = {i}, existe mas de {i} relación/nes de cointegración'
+        else:
+            sr = f'No hay suficiente evidencia para rechazar la hipótesis nula de r = {i}.'
+        print(f'r = {i}     {a95[i]}    {round(eig[i], 4)}    {sr}')
